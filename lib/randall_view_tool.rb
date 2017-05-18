fa0e0011bdd861a80ce205515c330222b84f96d0
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-05-17 20:18:00 -0700

    added code to rb file

--- lib/randall_view_tool.rb
+++ lib/randall_view_tool.rb
@@ -1,10 +1,3 @@
 require "randall_view_tool/version"
 require "randall_view_tool/renderer"
 
-module RandallViewTool
-	class Renderer
-		def self.copyright name, msg
-			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-		end
-	end
-end
--- lib/randall_view_tool/render.rb
+++ lib/randall_view_tool/render.rb
@@ -1,3 +1,7 @@
 module RandallViewTool
-  # Your code goes here...
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
